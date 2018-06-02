0347a4a17f1af94a5f893f16b1fe52e4e422fb20
Artem Musalitin <musalitin.a.a@gmail.com>
2018-06-02 12:36:08 +0400

    add test_replase solution

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,6 +2,25 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
+        max = array[0]
+
+        def findMax(value, max)
+          if value > max
+            max = value
+          end
+          return max
+        end
+
+        for i in 0..(array.length()-1)
+          max = findMax(array[i],max)
+        end
+
+        for i in 0..(array.length()-1)
+          if array[i]>0
+            array[i] = max
+          end
+        end
+
         array
       end
 
