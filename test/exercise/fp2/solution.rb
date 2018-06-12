e4518f84482de870218caf16a3a786088ccf1a56
Artem Musalitin <musalitin.a.a@gmail.com>
2018-06-12 13:54:48 +0400

    rubocop offenses fixes

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,38 +5,36 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each(&block)
-        for i in 0..self.length-1
-          block.call(self[i])
+      def my_each(*)
+        for i in 0..(self.length - 1)
+          yield(self[i])
         end
         self
       end
 
       # Написать свою функцию my_map
-      def my_map(&block)
-        answer=self.class.new
-        for i in 0..self.length-1
-          answer << block.call(self[i])
+      def my_map(*)
+        answer = self.class.new
+        for i in 0..(self.length - 1)
+          answer << yield(self[i])
         end
         answer
       end
 
       # Написать свою функцию my_compact
       def my_compact
-        answer=self.class.new
-        for i in 0..self.length-1
-          if self[i] != nil
-            answer << self[i]
-          end
+        answer = self.class.new
+        for i in 0..(self.length - 1)
+          answer << self[i] unless self[i].nil?
         end
         answer
       end
 
       # Написать свою функцию my_reduce
-      def my_reduce(memo=0, &block)
-        memo = 1 if block.call(memo, rand(1..100)) == 0
-        for i in 0..self.length-1
-          memo = block.call(memo, self[i])
+      def my_reduce(memo = 0, *)
+        memo = 1 if memo == 0 && yield(memo, rand(1..100)) == 0
+        for i in 0..(self.length - 1)
+          memo = yield(memo, self[i])
         end
         memo
       end
