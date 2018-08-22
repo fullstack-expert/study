f6b216ea6ddf783e479cd086bec16fb0c25e3088
7R41N33 <chepenkoanton2011@yandex.ru>
2018-08-22 21:57:13 +0400

    array custom methods

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -6,18 +6,34 @@ module Exercise
 
       # Написать свою функцию my_each
       def my_each
+        for elem in self do
+          yield(elem)
+        end
       end
 
       # Написать свою функцию my_map
       def my_map
+        new_arr = MyArray.new
+        my_each { |elem| new_arr << yield(elem) }
+        new_arr
       end
 
       # Написать свою функцию my_compact
       def my_compact
+        new_arr = MyArray.new
+        my_each { |elem| new_arr << elem unless elem.nil? }
+        new_arr
       end
 
       # Написать свою функцию my_reduce
-      def my_reduce
+      def my_reduce(initial = nil)
+        arr = initial.nil? ? self[1, size] : self
+        return initial if arr.nil?
+
+        acc = initial || self[0]
+
+        arr.my_each { |elem| acc = yield(acc, elem) }
+        acc
       end
     end
   end
