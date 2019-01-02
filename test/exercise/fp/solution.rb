cf66c711cf18503e39fa78459ad04c90ee51df9d
Zipofar <admin@zipofar.ru>
2019-01-02 19:05:17 +0300

    Done fp rating

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,7 +5,20 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(_array)
-        0
+        i = 0
+        sumOfRating = _array.reduce(0) { |memo, film|
+          rating = film["rating_kinopoisk"]
+          country = film["country"]
+          numOfCountries = country.nil? ? 0 : country.split(',').length
+
+          if rating.nil? || rating.to_f == 0 || numOfCountries < 2
+            next memo
+          end
+
+          i += 1
+          memo += rating.to_f
+        }
+        return sumOfRating / i
       end
 
       def chars_count(_films, _threshold)
--- test/exercise/fp/test.rb
+++ test/exercise/fp/test.rb
@@ -7,10 +7,11 @@ class Exercise::FpTest < Minitest::Test
   # Посчитать средний рейтинг фильмов по версии кинопоиска у которых две или больше стран
   # Фильмы у которых рейтиг не задан или равен 0 не учитывать в расчете среднего.
   def test_rating
-    skip
+#   skip
     array = CSV.readlines('./test/fixtures/films.csv', headers: true)
 
     result = Exercise::Fp.rating(array)
+    puts "---#{result}---"
     assert result == 6.809410385259628
   end
 
