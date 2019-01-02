acde6619c4e912cd22ed9cd03d85d751b76c69b8
Zipofar <admin@zipofar.ru>
2019-01-02 16:19:38 +0300

    Test arrays done

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,31 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max = array.max
+        array.map { |i| i < max && i >= 0 ? max : i  }
       end
 
       def search(_array, _query)
-        0
+		  startIndex = 0
+		  lastIndex = _array.length - 1
+
+		  while true do
+			leftLastIndex = lastIndex / 2
+			rightLastIndex = lastIndex
+
+			if _query == _array[leftLastIndex]
+			  return leftLastIndex
+			elsif _query == _array[rightLastIndex]
+			  return rightLastIndex
+			elsif rightLastIndex - leftLastIndex < 1
+			  return -1
+			elsif _query > _array[leftLastIndex]
+			  startIndex = leftLastIndex + 1
+			  lastIndex -= 1
+			elsif _query < _array[leftLastIndex]
+			  lastIndex = leftLastIndex - 1
+			end
+		  end
       end
     end
   end
