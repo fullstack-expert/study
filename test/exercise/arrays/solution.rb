411cc96dc71ac08d32d65fab08ef83e44c970b63
Zipofar <admin@zipofar.ru>
2019-01-04 00:05:14 +0300

    Refactor array

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -7,26 +7,26 @@ module Exercise
       end
 
       def search(_array, _query)
-		  startIndex = 0
-		  lastIndex = _array.length - 1
+        startIndex = 0
+        lastIndex = _array.length - 1
 
-		  while true do
-			leftLastIndex = lastIndex / 2
-			rightLastIndex = lastIndex
+        while true do
+          leftLastIndex = lastIndex / 2
+          rightLastIndex = lastIndex
 
-			if _query == _array[leftLastIndex]
-			  return leftLastIndex
-			elsif _query == _array[rightLastIndex]
-			  return rightLastIndex
-			elsif rightLastIndex - leftLastIndex < 1
-			  return -1
-			elsif _query > _array[leftLastIndex]
-			  startIndex = leftLastIndex + 1
-			  lastIndex -= 1
-			elsif _query < _array[leftLastIndex]
-			  lastIndex = leftLastIndex - 1
-			end
-		  end
+          if _query == _array[leftLastIndex]
+            return leftLastIndex
+          elsif _query == _array[rightLastIndex]
+            return rightLastIndex
+          elsif rightLastIndex - leftLastIndex < 1
+            return -1
+          elsif _query > _array[leftLastIndex]
+            startIndex = leftLastIndex + 1
+            lastIndex -= 1
+          elsif _query < _array[leftLastIndex]
+            lastIndex = leftLastIndex - 1
+          end
+        end
       end
     end
   end
