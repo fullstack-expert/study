368b6b408d26a91a2a34f61dbf398c0c71583872
LKS007 <leon99_21@mail.ru>
2019-01-03 19:11:17 +0400

    Add fp primitives

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -6,18 +6,35 @@ module Exercise
 
       # Написать свою функцию my_each
       def my_each
+        for x in self
+          yield(x)
+        end
       end
 
       # Написать свою функцию my_map
       def my_map
+        new_map_array = MyArray.new
+        for x in self
+          new_map_array.push(yield(x))
+        end
+        new_map_array
       end
 
       # Написать свою функцию my_compact
       def my_compact
+        new_compact_array = MyArray.new
+        for x in self
+          new_compact_array.push(x) unless x.nil?
+        end
+        new_compact_array
       end
 
       # Написать свою функцию my_reduce
-      def my_reduce
+      def my_reduce(acc = nil)
+        for x in self
+          acc = acc ? yield(acc, x) : x
+        end
+        acc
       end
     end
   end
