25f66484ce81b107d3b9e6dec76904a50cd79740
Zipofar <admin@zipofar.ru>
2019-01-05 17:29:58 +0300

    Done rackup test

--- test/exercise/rackup/inatra.rb
+++ test/exercise/rackup/inatra.rb
@@ -1,9 +1,27 @@
 module Inatra
   class << self
+    @@routes = []
     def routes
+      yield
+    end
+
+    def get(path, &block)
+      @@routes << { path: path, block: block }
     end
 
     def call(env)
+      path = env['PATH_INFO']
+      @@routes.each { |route| return route[:block].call if path == route[:path] }
     end
   end
+  module Delegator
+    def self.delegate
+      define_method('get') do |path, &block|
+        return Inatra.get(path, &block)
+      end
+    end
+    delegate
+  end
 end
+
+extend Inatra::Delegator
