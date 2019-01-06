cd4eb93715f5552c7fe7d44c2734341d4c1221f4
LKS007 <leon99_21@mail.ru>
2019-01-06 23:57:30 +0400

    add rackup exercise

--- test/exercise/rackup/inatra.rb
+++ test/exercise/rackup/inatra.rb
@@ -1,9 +1,24 @@
 module Inatra
   class << self
-    def routes
+    def routes(&block)
+      @routes ||= {}
+      instance_eval(&block)
     end
 
     def call(env)
+      req_method = env['REQUEST_METHOD'].downcase.to_sym
+      return [404, {}, ['Not found']] if @routes[req_method][env['PATH_INFO']].nil?
+      @routes[req_method][env['PATH_INFO']].call
+    end
+
+    def get(path, &block)
+      @routes[:get] ||= {}
+      @routes[:get][path] = block
+    end
+
+    def post(path, &block)
+      @routes[:post] ||= {}
+      @routes[:post][path] = block
     end
   end
 end
