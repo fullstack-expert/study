90134b769ad58d0f51623b4dbe0018c3bf83a02d
Vasiliy Krasnobay <krasnobay@users.noreply.github.com>
2019-01-15 23:02:42 +0400

    implemented Exercise::Arrays.replace

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,6 +2,10 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
+        max = array.max
+        for i in 0..array.length - 1
+          array[i] = max if array[i] > 0
+        end
         array
       end
 
