a4620e888b7d76ed02011cb7c194c2549d82496b
Vasiliy Krasnobay <krasnobay@users.noreply.github.com>
2019-01-18 19:20:44 +0400

    implemented Exercise::Fp.rating

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,8 +4,14 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        filtered = array.select do |film|
+          !film['country'].nil? &&
+            film['country'].split(',').length >= 2 &&
+            !film['rating_kinopoisk'].nil? &&
+            film['rating_kinopoisk'].to_f > 0
+        end
+        filtered.map { |e| e['rating_kinopoisk'].to_f }.reduce(:+) / filtered.length
       end
 
       def chars_count(_films, _threshold)
