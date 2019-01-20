46b72425755883ddc12f2dca579657f357562bdb
Vasiliy Krasnobay <krasnobay@users.noreply.github.com>
2019-01-20 23:34:48 +0400

    implemented exercise/rackup

--- test/exercise/rackup/inatra.rb
+++ test/exercise/rackup/inatra.rb
@@ -1,9 +1,21 @@
 module Inatra
   class << self
-    def routes
+    def method_missing(method_name, *args, &block)
+      path = args.first.to_s
+      method = method_name.upcase.to_s
+      @routes[path] ||= {}
+      @routes[path][method] = block
+    end
+
+    def routes(&block)
+      @routes ||= {}
+      instance_eval(&block)
     end
 
     def call(env)
+      path = env['PATH_INFO']
+      method = env['REQUEST_METHOD'].upcase.to_s
+      @routes[path][method].call(env)
     end
   end
 end
