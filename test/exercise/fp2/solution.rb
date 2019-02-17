7ca1ff9de445cb7cfaa0ab0df856d2f659f6f5a7
recroot <recroot89@gmail.com>
2019-02-17 23:45:27 +0500

    solve fp2

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -6,18 +6,36 @@ module Exercise
 
       # Написать свою функцию my_each
       def my_each
+        for element in self
+          yield element
+        end
       end
 
       # Написать свою функцию my_map
       def my_map
+        arr = self.class.new
+        my_each { |element| arr << yield(element) }
+        arr
       end
 
       # Написать свою функцию my_compact
       def my_compact
+        arr = self.class.new
+        my_each { |element| arr << element unless element.nil? }
+        arr
       end
 
       # Написать свою функцию my_reduce
-      def my_reduce
+      def my_reduce(init = nil)
+        acc = init
+        my_each do |element|
+          if acc.nil?
+            acc = element
+          else 
+            acc = yield(acc, element)
+          end
+        end
+        acc
       end
     end
   end
