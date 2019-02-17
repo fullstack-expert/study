fa2e7c381cec73304435b89bb27028c2b85fbbe5
recroot <recroot89@gmail.com>
2019-02-17 23:41:34 +0500

    solving arrays

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,14 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        array.map { |x| x.positive? ? 100 : x }
       end
 
       def search(_array, _query)
-        0
+        _array.each do |x|
+          return _array.find_index(x) if x == _query
+        end
+        -1
       end
     end
   end
