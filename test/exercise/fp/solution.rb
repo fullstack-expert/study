8176193eb0abdd642ddca1355b70177527ac57e3
Freitag <nepiz@i.ua>
2019-03-05 19:28:46 +0400

    i *heart* kinopoisk

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,12 +4,15 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        country = array.select { |film| film['country'].to_s.split(',').length >= 2 }
+        rating = country.select { |film| film['rating_kinopoisk'].to_f >= 2 }
+        (rating.map { |film| film['rating_kinopoisk'].to_f }).reduce(:+) / rating.length
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def chars_count(array, rating)
+        films = array.select { |film| film['rating_kinopoisk'].to_f >= rating }
+        (films.map { |film| film['name'].count('и') }).reduce(:+)
       end
     end
   end
