799ca30b138126ce3c47f5b50fa82bcfe2637297
Freitag <nepiz@i.ua>
2019-03-05 15:13:52 +0400

    arrays done_rubocop fixes

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -6,41 +6,34 @@ module Exercise
         puts new_array
         new_array
       end
+
       def positive_to_max(array)
         max = array.max
         array.map do |n|
-          n > 0 ? max : n
+          n.positive? ? max : n
         end
       end
-      
+
       def search(array, query)
-        
-        if array.length == 0
-          return - 1
-        end  
+        return - 1 if array.empty?
+
         left = 0
         right = array.length
         mid = 0
-        
 
-       while left < right
-         mid = left + ((right - left) / 2).ceil
-       
-          if array[mid] == query
-              return mid
-          end 
-          
-         return -1 if right == 0 || left == array.length - 1
-      
+        while left < right
+          mid = left + ((right - left) / 2).ceil
+          return mid if array[mid] == query
+          return -1 if right.zero? || left == array.length - 1
+
           if array[mid] > query
-              right = mid
+            right = mid
           else
-              left = mid + 1
-        
+            left = mid + 1
           end
         end
         -1
-      end  
+      end
     end
   end
-end 
+end
