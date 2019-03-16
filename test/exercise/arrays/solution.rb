05fc05239de25c32750606c890f2f58c37b4b613
ramazan yaraliev <yaru@bk.ru>
2019-03-16 18:27:58 +0300

    completed arrays

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,28 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max_item = array.max
+        array.map { |item| item.positive? ? max_item : item }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        left = 0
+        right = array.length - 1
+
+        while left <= right
+          mid = (left + right) / 2
+          guess = array[mid]
+
+          return mid if guess == query
+
+          if guess < query
+            left = mid + 1
+          else
+            right = mid - 1
+          end
+        end
+
+        -1
       end
     end
   end
