99256bcd960c4c4bf594b156c8294044066d4896
greybutton <greybutton@yandex.com>
2019-03-16 20:48:36 +0800

    done arrays

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,26 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max = array.max
+        array.collect { |num| num >= 0 ? max : num }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        left = -1
+        right = array.length
+        while right > left + 1
+          middle = ((right + left) / 2).floor
+          if array[middle] < query
+            left = middle
+          else
+            right = middle
+          end
+        end
+        if right < array.length && array[right] == query
+          right
+        else
+          -1
+        end
       end
     end
   end
