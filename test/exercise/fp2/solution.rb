b46fe821bc7fd1c517f6196bf51bd200e9a89bbc
ramazan yaraliev <yaru@bk.ru>
2019-03-17 20:27:58 +0300

    completed fp2

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -6,18 +6,71 @@ module Exercise
 
       # Написать свою функцию my_each
       def my_each
+        index = 0
+        length = self.length
+
+        loop do
+          break if index == length
+
+          yield(self[index])
+          index += 1
+        end
+
+        self
       end
 
       # Написать свою функцию my_map
       def my_map
+        index = 0
+        length = self.length
+        new_array = MyArray.new
+
+        loop do
+          break if index == length
+
+          new_array << yield(self[index])
+          index += 1
+        end
+
+        new_array
       end
 
       # Написать свою функцию my_compact
       def my_compact
+        index = 0
+        length = self.length
+        new_array = MyArray.new
+
+        loop do
+          break if index == length
+
+          if block_given?
+            block_result = yield(self[index])
+            new_array << block_result if block_result
+          else
+            self[index].nil? ? new_array : new_array << self[index]
+          end
+
+          index += 1
+        end
+
+        new_array
       end
 
       # Написать свою функцию my_reduce
-      def my_reduce
+      def my_reduce(acc = nil)
+        index = 1
+        length = self.length
+        acc ||= first
+
+        loop do
+          break if index == length
+
+          acc = yield(acc, self[index])
+          index += 1
+        end
+
+        acc
       end
     end
   end
