4fc250ae6202d1ee52c1782860bd1220ac6a27ba
Aleksandr Ohotnikov <aleksohotnikov@gmail.com>
2019-03-19 00:55:19 +0400

    arrays test dome

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,35 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
-      end
+        def find_max(_arr)
+          max = _arr[0]
 
+        _arr.each do |i|
+          max = max < i ? i : max
+        end
+
+        max
+        end
+        max_elemen = find_max(array)
+        array.map { |elem| elem > 0 ? array.max : elem }
+      end
       def search(_array, _query)
-        0
+        low = 0
+        high = _array.size - 1
+
+        while low <= high
+          mid = (low + high) / 2
+          guess = _array[mid]
+          return mid if guess == _query
+
+          if guess > _query
+            high = mid - 1
+          else
+            low = mid + 1
+          end
+        end
+
+        -1
       end
     end
   end
--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,12 +4,20 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        filtered_films = array.select do |film|
+          film['rating_kinopoisk'].to_f.positive? && film['country'].to_s.split(',').count > 1
+        end
+
+        ratings_sum = filtered_films.reduce(0) { |sum, film| sum + film['rating_kinopoisk'].to_f }
+
+        ratings_sum / filtered_films.count
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def chars_count(films, threshold)
+        filtered_films = films.select { |film| film['rating_kinopoisk'].to_f >= threshold }
+
+        filtered_films.reduce(0) { |sum, film| sum + film['name'].scan(/и/).count }
       end
     end
   end
