8d59f8de68dad7d0ec8e67b31d44789417574fea
Bulat Nasibullin <nasibullinbulat95@gmail.com>
2019-03-22 12:07:49 +0400

    Added solution fp task

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,12 +4,43 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        new_array = []
+
+        array.map do |film|
+
+          if(film["rating_kinopoisk"].to_f.positive? &&
+              film["country"] != nil &&
+              film["country"].split(',').length >= 2
+          )
+
+            new_array.push(film["rating_kinopoisk"].to_f)
+
+          end
+        end
+
+        arr_length = new_array.length
+        result = new_array.reduce(:+)
+
+        return result/arr_length
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def chars_count(films, threshold)
+        count = 0
+
+        films.map do |film|
+
+          if(film["rating_kinopoisk"].to_f >= threshold)
+            arr = film["name"].split('')
+
+            count += arr.reduce(0) {|sum, char|
+              (char == 'и') && sum += 1
+              sum
+            }
+          end
+        end
+
+        return count
       end
     end
   end
