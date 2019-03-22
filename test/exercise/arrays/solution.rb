66c042ed25556d11550ec3cb6a999dba239bf9a9
Bulat Nasibullin <nasibullinbulat95@gmail.com>
2019-03-22 08:58:08 +0400

    Added solution arrays tasks

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,38 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max_elem_of_array = array.max
+        new_array = []
+
+        array.each {|item|
+          (item > 0) ? new_array.push(max_elem_of_array) : new_array.push(item)
+        }
+
+        return new_array
       end
 
       def search(_array, _query)
-        0
+
+        low = 0
+        high = _array.length - 1
+
+        while(low <= high)
+
+          middle = (low+high) / 2
+          item = _array[middle]
+
+          if(item == _query)
+            return middle
+          elsif(item > _query)
+            high = middle - 1
+          else
+            low = middle + 1
+          end
+
+        end
+
+        return -1
+
       end
     end
   end
