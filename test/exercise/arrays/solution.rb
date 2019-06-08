b07ceffe9869a6a721d7fa06d7625bb897f0380f
GrigoryCode <grigorycode@gmail.com>
2019-06-08 14:36:20 +0300

    Array test

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -1,13 +1,34 @@
 module Exercise
   module Arrays
     class << self
+      
       def replace(array)
-        array
+        array.index(array.first).upto(array.rindex(array.last)) do |i|
+          if array[i] >= 0
+            array[i] = array.max
+          end
+        end
       end
 
-      def search(_array, _query)
-        0
+      def search(array, e)
+        mid = array.size / 2
+        low = 0
+        high = array.size - 1
+        while array[mid] != e and low < high do
+          if e > array[mid]
+            low = mid + 1
+          else
+            high = mid - 1
+          end
+          mid = (low + high) / 2
+        end
+        if e != array[mid]
+          return -1
+        else
+          return mid
+        end
       end
+
     end
   end
 end
