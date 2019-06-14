2a27cda15601af214dc34c5273bb988a21c8b9f8
Oleg Ivanov <gluk.main+github@gmail.com>
2019-06-14 20:05:18 +0300

    replace & search

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,26 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max = array.max
+        array.map { |e| e.positive? ? max : e }
       end
 
       def search(_array, _query)
-        0
+        i = 0
+        j = _array.length - 1
+
+        while i <= j
+          ind = (i + j) / 2
+          return ind if _array[ind] == _query
+
+          if _query > _array[ind]
+            i = ind + 1
+          else
+            j = ind - 1
+          end
+        end
+
+        -1
       end
     end
   end
