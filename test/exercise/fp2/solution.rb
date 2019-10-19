59c6b62b06aaac37c2cab3139caea13f17caa762
Petr Ablyazov <phoenix-h@mail.ru>
2019-10-19 22:53:12 +0400

    Fp2. My Each

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -6,10 +6,15 @@ module Exercise
 
       # Написать свою функцию my_each
       def my_each
+        for item in self
+          yield(item)
+        end
+        self
       end
 
       # Написать свою функцию my_map
       def my_map
+
       end
 
       # Написать свою функцию my_compact
