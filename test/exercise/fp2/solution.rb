f4e7b248877784680f111b6d3b40f8ff49c36266
Petr Ablyazov <phoenix-h@mail.ru>
2019-10-20 21:31:49 +0400

    Fp2. My map

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -14,7 +14,9 @@ module Exercise
 
       # Написать свою функцию my_map
       def my_map
-
+        result = MyArray.new()
+        self.my_each{|val| result << yield(val)}
+        result
       end
 
       # Написать свою функцию my_compact
