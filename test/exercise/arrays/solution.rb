7bac582eef2a0a414c01b1eb4dd21a9d1ad4f82c
Ivan <evan4mc@gmail.com>
2019-10-30 11:03:15 +0500

    array test pass

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,23 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max = array.max
+        array.map {|x| x > 0 ? max : x }
       end
 
       def search(_array, _query)
-        0
+
+        if(_array.empty?)
+          return -1
+        end
+
+        res = _array.index(_query)
+        
+        if(res.nil?)
+          return -1
+        else
+          return res
+        end
       end
     end
   end
