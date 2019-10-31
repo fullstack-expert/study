2e2be4ca9a4ef8a01df6ee0bc00f202d77501ddb
Aynur Galimov <galim-aynur@ya.ru>
2019-10-31 20:51:17 +0500

    Add solutions for arrays

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,26 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        array.map { |a| a > 0 ? array.max : a }
       end
 
       def search(_array, _query)
-        0
+        low = 0
+        high = _array.length - 1
+
+        while low <= high
+          mid = (low + high) / 2
+          guess = _array[mid]
+          if guess == _query
+            return mid
+          end
+          if guess > _query
+            high = mid - 1
+          else
+            low = mid + 1
+          end
+        end
+        return -1
       end
     end
   end
