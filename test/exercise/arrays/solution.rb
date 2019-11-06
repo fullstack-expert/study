4401ebad9963aa8ec32f1b63af915459398a8878
Sergey Edinin <baboot.dev@gmail.com>
2019-11-06 11:46:31 +0400

    arrays test

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,12 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        array.map { |x| x > 0 ? array.max : x }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        index = array.index(query)
+        index.nil? ? -1 : index
       end
     end
   end
