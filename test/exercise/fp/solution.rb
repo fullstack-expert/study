2fc1c9eba81ed918ef84a447605ad69f7fd7cd0c
Sergey Edinin <baboot.dev@gmail.com>
2019-11-06 13:41:41 +0400

    rating tas

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -2,10 +2,14 @@ module Exercise
   module Fp
     class << self
       # Обратиться к параметрам фильма можно так:
-      # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
-      # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      # film['name'], film['rating_kinopoisk'], film['rating_imdb'],
+      # film['genres'], film['year'], film['access_level'], film['country']
+      def rating(array)
+        filtered = array.select do |film|
+          !film['rating_kinopoisk'].nil? && film['rating_kinopoisk'].to_f.positive? && !film['country'].nil? && film['country'].split(',').length >= 2
+        end
+
+        filtered.map { |film| film['rating_kinopoisk'].to_f }.reduce(:+) / filtered.length
       end
 
       def chars_count(_films, _threshold)
