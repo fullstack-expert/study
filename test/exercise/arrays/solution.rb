c3c36df267e90b9dbf3ebf9e7ffa633bd08b8906
Igor Klein <89021282507@mail.ru>
2019-11-21 12:37:08 +0400

    the second solution of the problem

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -1,23 +1,33 @@
 module Exercise
   module Arrays
     class << self
-      def replace(array)        
-        maxValue = array.max
-        newArray = array.map {|num|
-             if num > 0
-                num = maxValue
-             else mum = num
-             end}
-        return newArray         
+      def replace(array)
+        index_arr = 0
+        max_value = array[index_arr]
+        a = array.length - 1
+        while index_arr < a
+          max_value = array[index_arr + 1] if array[index_arr] < array[index_arr + 1] && array[index_arr + 1] > max_value
+          index_arr += 1
+        end
+        index_arr = 0
+        while index_arr < array.length
+          array[index_arr] = max_value if (array[index_arr]).positive?
+          index_arr += 1
+        end
+        array
       end
 
-      def search(_array, _query)
-        indexArray =_array.bsearch_index {|x| _query <=> x } 
-        if indexArray == nil
-          return -1        
-        else
-          return indexArray        
+      def search(array, query)
+        first_index = 0
+        last_index = array.length - 1
+        while first_index <= last_index
+          middle_index = (first_index + last_index) / 2
+          return middle_index if array[middle_index] == query
+
+          first_index = middle_index + 1 if array[middle_index] < query
+          last_index = middle_index - 1 if array[middle_index] > query
         end
+        -1
       end
     end
   end
