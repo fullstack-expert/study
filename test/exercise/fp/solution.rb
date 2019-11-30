b42959788abd11573ec4bfd1e29a5b830a7c2957
ilya <sad1lovbatya@gmail.com>
2019-11-30 15:10:10 -0500

    test complete

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,13 +4,24 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      
+      def rating(array)
+        sort_array = array.find_all { |film| film["country"].to_s.include?(",") && film["rating_kinopoisk"].to_f > 0 && !film['rating_kinopoisk'].nil?  }
+        rating = sort_array.map { |film| film["rating_kinopoisk"].to_f }
+        sum = rating.reduce(0) { |sum, num| sum + num }
+        sum / rating.length
       end
 
-      def chars_count(_films, _threshold)
-        0
+     
+
+      def chars_count(films, threshold)
+        sort_array = films.find_all { |film| film["rating_kinopoisk"].to_f >= threshold.to_f}
+        sort_by_name = sort_array.map {|film| film["name"]}
+        sort_by_name.reduce(0) {|sum , film| sum + film.count('и') }
       end
     end
   end
 end
+
+
+#   Посчитать количесвто букв 'и' в названиях всех фильмов с рейтингом кинопоиска больше или равным заданному значению
