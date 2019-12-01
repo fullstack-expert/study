23ed2fe32e083ea8f9a8e93ff2363027a03adaf8
ilya <sad1lovbatya@gmail.com>
2019-12-01 14:28:17 -0500

    redacted

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -1,43 +1,31 @@
 module Exercise
   module Arrays
     class << self
-      def replace(array)
-        i = 1
-        max = array[0]
-        while i < array.length
-          max = array[i] if array[i] > max 
-          i += 1
+      def my_max(array)
+        array.reduce(0) do |acc,elem|
+        acc < elem ? elem : acc
         end
-        max_arr = max
- 
-        j = 0
-        while j < array.length
-          array[j] = if array[j] > 0
-          max_arr
-          else
-            array[j]
-          end
-          j += 1
-        end
-        array
-end
+      end 
 
-      def search(array,num)
-        low = 0
-        high = array.length - 1 
-        while low <= high do 
-          mid = (low + high) / 2 
-          guess = array[mid]
-        if guess == num 
-             mid 
-        elsif guess > num 
-            high = mid - 1
-        else 
-            low = mid + 1 
-        end 
-        end
-        -1 
+      def replace(array)
+        array.map do |num|
+          num > 0 ? my_max(array) : num
       end
+  end
+  
+
+  def search(array, num, low=0, high=array.length-1)  
+      return -1 if low > high  
+      mid = (low + high) / 2  
+      return mid if array[mid]==num 
+      if array[mid] > num  
+        high = mid - 1  
+      else  
+        low = mid + 1  
+      end  
+      search(array, num, low, high)   
+      end  
+
     end
   end
 end
