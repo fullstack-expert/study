489911cfe4c569f5fd11be81691ab54eaa30405d
ilya <sad1lovbatya@gmail.com>
2019-12-03 18:03:07 -0500

    example

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -5,38 +5,38 @@ module Exercise
         i = 1
         max = array[0]
         while i < array.length
-          max = array[i] if array[i] > max 
+          max = array[i] if array[i] > max
           i += 1
         end
         max_arr = max
- 
+
         j = 0
         while j < array.length
           array[j] = if array[j] > 0
-          max_arr
-          else
-            array[j]
+                       max_arr
+                     else
+                       array[j]
           end
           j += 1
         end
         array
 end
 
-      def search(array,num)
+      def search(array, num)
         low = 0
-        high = array.length - 1 
-        while low <= high do 
-          mid = (low + high) / 2 
+        high = array.length - 1
+        while low <= high
+          mid = (low + high) / 2
           guess = array[mid]
-        if guess == num 
-             mid 
-        elsif guess > num 
+          if guess == num
+            mid
+          elsif guess > num
             high = mid - 1
-        else 
-            low = mid + 1 
-        end 
+          else
+            low = mid + 1
+          end
         end
-        -1 
+        -1
       end
     end
   end
