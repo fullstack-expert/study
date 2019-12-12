7b857ec701d02ca232285ee8dc4f31b3e12aa69f
EgorLes <egor.lenivin@gmail.com>
2019-12-12 13:27:46 +0300

    rubocop fixes

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -6,28 +6,24 @@ module Exercise
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(array)
         array_raitings_with_nil = array.map do |film|
-            if film['country'].to_s.include?(',') && film['rating_kinopoisk'].to_f.positive?
-               film['rating_kinopoisk'].to_f
-            end
+          if film['country'].to_s.include?(',') && film['rating_kinopoisk'].to_f.positive?
+            film['rating_kinopoisk'].to_f
+          end
         end
 
-        array_raitings = array_raitings_with_nil.select { |num| num != nil }
+        array_raitings = array_raitings_with_nil.select { |num| !num.nil? }
 
         sum_rate = array_raitings.reduce(:+)
 
-        middle_rate = sum_rate/array_raitings.length
+        middle_rate = sum_rate / array_raitings.length
       end
 
       def chars_count(films, threshold)
-        array_films_names_with_nil = films.map do |film|
-            if film['rating_kinopoisk'].to_f >= threshold
-               film['name'].to_s
-            end
-        end
+        array_films_names_with_nil = films.map { |film| film['name'].to_s if film['rating_kinopoisk'].to_f >= threshold }
 
-        array_films_names = array_films_names_with_nil.select { |name| name != nil }
+        array_films_names = array_films_names_with_nil.select { |name| !name.nil? }
 
-        number_chars_in_every_name = array_films_names.map { |name| name.count('и')}
+        number_chars_in_every_name = array_films_names.map { |name| name.count('и') }
 
         result = number_chars_in_every_name.reduce(:+)
       end
