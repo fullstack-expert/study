2e0bbc87ab7c17c60b46f278832921b9262bf478
EgorLes <egor.lenivin@gmail.com>
2019-12-13 11:27:20 +0300

    raiting -> rating; reduce, map

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,27 +5,23 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(array)
-        array_raitings_with_nil = array.map do |film|
-          if film['country'].to_s.include?(',') && film['rating_kinopoisk'].to_f.positive?
-            film['rating_kinopoisk'].to_f
-          end
-        end
+        films_with_two_countries_and_rating = array.select { |film| film['country'].to_s.include?(',') && film['rating_kinopoisk'].to_f.positive? }
 
-        array_raitings = array_raitings_with_nil.select { |num| !num.nil? }
+        array_ratings = films_with_two_countries_and_rating.map { |film| film['rating_kinopoisk'].to_f }
 
-        sum_rate = array_raitings.reduce(:+)
+        sum_rate = array_ratings.reduce(:+)
 
-        middle_rate = sum_rate / array_raitings.length
+        sum_rate / array_ratings.length
       end
 
       def chars_count(films, threshold)
-        array_films_names_with_nil = films.map { |film| film['name'].to_s if film['rating_kinopoisk'].to_f >= threshold }
+        films_rating_kinopoisk_more_then_threshold = films.select { |film| film['rating_kinopoisk'].to_f >= threshold }
 
-        array_films_names = array_films_names_with_nil.select { |name| !name.nil? }
+        films_names = films_rating_kinopoisk_more_then_threshold.map { |film| film['name'].to_s }
 
-        number_chars_in_every_name = array_films_names.map { |name| name.count('и') }
+        number_chars_in_every_name = films_names.map { |name| name.count('и') }
 
-        result = number_chars_in_every_name.reduce(:+)
+        number_chars_in_every_name.reduce(:+)
       end
     end
   end
