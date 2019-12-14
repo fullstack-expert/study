ff80165e87d3bce71a246c2a0b329e2789a4a957
EgorLes <egor.lenivin@gmail.com>
2019-12-14 11:37:22 +0300

    my_each my_map my_compact my_reduce

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -1,23 +1,37 @@
 module Exercise
   module Fp2
     class MyArray < Array
-      # Использовать стандартные функции массива для решения задач нельзя.
-      # Использовать свои написанные функции для реализации следующих - можно.
+    # Использовать стандартные функции массива для решения задач нельзя.
+    # Использовать свои написанные функции для реализации следующих - можно.
 
-      # Написать свою функцию my_each
+    # Написать свою функцию my_each
       def my_each
+        i = 0
+        while i < length
+          yield self[i]
+          i += 1
+        end
+        self
       end
 
       # Написать свою функцию my_map
       def my_map
+        new_array = MyArray.new
+        my_each { |el| new_array << yield(el) }
+        new_array
       end
 
       # Написать свою функцию my_compact
       def my_compact
+        new_array = MyArray.new
+        self.my_each { |el| new_array << el unless el.nil? }
+        new_array
       end
 
       # Написать свою функцию my_reduce
-      def my_reduce
+      def my_reduce(acc = nil)
+        my_each { |el| acc = acc ? yield(acc, el) : el }
+        acc
       end
     end
   end
