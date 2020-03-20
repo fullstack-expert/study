bd13793702374d38ba1345f9e80ebaabd2f08760
Artem Gapeevtsev <agapeevtsev@yandex.ru>
2020-03-20 10:14:10 +0800

    fp/rating

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,7 +5,19 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(_array)
-        0
+        filtered_films = _array
+          .select { |film|
+            countries = film["country"]
+            !countries.nil? && countries.split(' ').size >= 2
+          }.select { |film|
+            rating = film["rating_kinopoisk"].to_f
+            !rating.nil? && rating > 0.0
+          }
+
+        ratings = filtered_films.map { |film| film["rating_kinopoisk"].to_f }
+        sum_rating = ratings.reduce(0.0) { |sum, rating| sum += rating }
+
+        sum_rating / filtered_films.size
       end
 
       def chars_count(_films, _threshold)
