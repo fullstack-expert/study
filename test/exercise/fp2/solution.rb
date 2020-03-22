bc15a46f3688dff38335fadb85b33165313f429e
Artem Gapeevtsev <agapeevtsev@yandex.ru>
2020-03-22 10:35:03 +0800

    added my_each method

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -6,6 +6,9 @@ module Exercise
 
       # Написать свою функцию my_each
       def my_each
+        for item in self
+            yield(item)
+        end
       end
 
       # Написать свою функцию my_map
