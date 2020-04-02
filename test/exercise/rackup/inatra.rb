ea7ad5043fcdc3233f606916e4af08bf5e287efd
Artem Gapeevtsev <agapeevtsev@yandex.ru>
2020-04-02 09:22:59 +0800

    fix method_missing

--- test/exercise/rackup/inatra.rb
+++ test/exercise/rackup/inatra.rb
@@ -1,20 +1,13 @@
 module Inatra
     class << self
-        attr_accessor :route
-
         def routes(&block)
             instance_eval(&block)
         end
 
         def method_missing(method, url)
             method_name = method.to_s.upcase
-            @route = {} if @route.nil?
-
-            if @route.key?(method_name)
-                @route[method_name][url] = yield
-            else
-                @route[method_name] = { url => yield }
-            end
+            @route = {}
+            @route[method_name] = { url => yield }
         end
 
         def call(env)
