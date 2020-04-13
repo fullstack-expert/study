0541c6ada6ece2938ff26fcc35050c5b07e0291c
Tatiana Kalmykova <ul.tkalmykova@gmail.com>
2020-04-13 18:17:31 +0400

    implement replace

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,7 +2,11 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max_element = array.max
+        array.map do |element|
+          new_element = element > 0 ? max_element : element
+          new_element
+        end
       end
 
       def search(_array, _query)
