bae0fa317c395609b90aee0f539088628c70cb72
Vasilek <morphizm@ya.ru>
2020-04-14 19:58:40 +0300

    search

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -16,32 +16,21 @@ module Exercise
       def search(array, query)
         iter = lambda do |items, first_index, last_index|
           length = items.length
-          if items.length.zero?
-            return -1
-          end
-          half = length / 2
-          current = items[half]
-          z = last_index - half
-
-          if current == query
-            if length.even?
-              return z + 1
-            else
-              return z
-            end
-          elsif current < query
-            if length.even?
-              return iter.call(items.slice(half + 1, length), z + 2, last_index)
-            else
-              return iter.call(items.slice(half + 1, length), z, last_index)
-            end
+          return -1 if items.length.zero?
+          if length.even?
+            half = length / 2 - 1
+            z = last_index - half - 1
           else
-            if length.even?
-              return iter.call(items.slice(0, half), first_index, z)
-            else
-              return iter.call(items.slice(0, half), first_index, z - 1)
-            end
+            half = length / 2
+            z = last_index - half
           end
+          current = items[half]
+
+          return z if current == query
+
+          return iter.call(items.slice(half + 1, length), z + 1, last_index) if current < query
+
+          iter.call(items.slice(0, half), first_index, z - 1)
         end
         iter.call(array, 0, array.length - 1)
       end
