164d04354afed2d2b585257386071e92fc6407a3
Vasilek <morphizm@ya.ru>
2020-04-15 15:29:50 +0300

    own max, fix half

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,14 +2,10 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        max_value = array.max
+        max_value = max(array)
 
         array.map do |item|
-          if item >= 0
-            max_value
-          else
-            item
-          end
+          item >= 0 ? max_value : item
         end
       end
 
@@ -17,13 +13,10 @@ module Exercise
         iter = lambda do |items, first_index, last_index|
           length = items.length
           return -1 if items.length.zero?
-          if length.even?
-            half = length / 2 - 1
-            z = last_index - half - 1
-          else
-            half = length / 2
-            z = last_index - half
-          end
+
+          half = length / 2
+          z = length.even? ? (last_index - half + 1) : (last_index - half)
+
           current = items[half]
 
           return z if current == query
@@ -33,6 +26,12 @@ module Exercise
           iter.call(items.slice(0, half), first_index, z - 1)
         end
         iter.call(array, 0, array.length - 1)
+      end
+
+      def max(array)
+        array.reduce do |acc, value|
+          acc < value ? value : acc
+        end
       end
     end
   end
