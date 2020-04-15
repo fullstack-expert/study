74f29920990c33fc8a6e9bc8045e105197a624b3
Tatiana Kalmykova <ul.tkalmykova@gmail.com>
2020-04-15 08:55:15 +0400

    fix linter remarks

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -6,18 +6,18 @@ module Exercise
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(array)
         ratings = array
-          .reject{|film| film["country"] == nil || film["rating_kinopoisk"] == nil }
-          .select{|film| film["country"].split(',').size >= 2 }
-          .map{|film| film["rating_kinopoisk"].to_f}
-          .reject { |rating| rating == 0.0 }
+                  .reject { |film| film['country'].nil? || film['rating_kinopoisk'].nil? }
+                  .select { |film| film['country'].split(',').size >= 2 }
+                  .map { |film| film['rating_kinopoisk'].to_f }
+                  .reject { |rating| rating == 0.0 }
 
         result = ratings.reduce(:+) / ratings.size
         result
       end
 
       def chars_count(films, threshold)
-        target_films = films.select{|film| film["rating_kinopoisk"].to_f >= threshold}
-        result = target_films.reduce(0) { |acc, film| acc + film["name"].count('и')}
+        target_films = films.select { |film| film['rating_kinopoisk'].to_f >= threshold }
+        result = target_films.reduce(0) { |a, e| a + e['name'].count('и') }
         result
       end
     end
