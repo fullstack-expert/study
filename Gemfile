76aa4e58da56b7907d780f40a3e426e5798e6611
Tatiana Kalmykova <ul.tkalmykova@gmail.com>
2020-04-15 08:13:30 +0400

    implement rating

--- Gemfile
+++ Gemfile
@@ -9,3 +9,4 @@ gem 'rubocop', require: false
 gem 'rack'
 gem 'test-unit'
 gem 'rack-test'
+gem 'byebug'
--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,8 +4,14 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        ratings = array
+          .reject{|film| film["country"] == nil || film["rating_kinopoisk"] == nil }
+          .select{|film| film["country"].split(',').size >= 2 }
+          .map{|film| film["rating_kinopoisk"].to_f}
+          .reject { |rating| rating == 0.0 }
+
+        ratings.reduce(:+) / ratings.size
       end
 
       def chars_count(_films, _threshold)
