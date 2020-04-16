b9a99a145c6eb9fc49b850b69d7e5a0478fe3b6e
Vasilek <morphizm@ya.ru>
2020-04-16 16:34:42 +0300

    rewrite

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,13 +5,12 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(films)
-        array_films = films.to_a.slice(1, films.count)
-
         filtered =
-          array_films
-          .select { |film| film[1].to_f > 0.0 }
-          .select { |film| film[6].to_s.split(',').length >= 2 }
-          .map { |film| film[1].to_f }
+          films
+          .by_row
+          .select { |film| film['rating_kinopoisk'].to_f > 0.0 }
+          .select { |film| film['country'].to_s.split(',').length >= 2 }
+          .map { |film| film['rating_kinopoisk'].to_f }
 
         sum = filtered.reduce(0, &:+)
 
@@ -19,18 +18,11 @@ module Exercise
       end
 
       def chars_count(films, threshold)
-        array_films = films.to_a.slice(1, films.count)
-
-        filtered =
-          array_films
-          .select { |film| film[1].to_f > threshold }
-          .map { |film| film[0] }
-
-        filtered.reduce(0) { |a, e| a + count(e, 'и') }
-      end
-
-      def count(str, symbol)
-        str.split('').count(symbol)
+        films
+          .by_row
+          .select { |film| film['rating_kinopoisk'].to_f > threshold }
+          .map { |film| film['name'] }
+          .reduce(0) { |a, e| a + e.count('и') }
       end
     end
   end
