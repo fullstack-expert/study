80126a22b66523f88c72e2626da22b57ab976bca
d.glebov <d.glebov@gmail.com>
2020-04-16 18:05:37 +0300

    solution array1

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,32 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+
+        max_arr = array.max
+        i = 0
+        while i < array.length do
+          if array[i] > 0
+            array[i] = max_arr
+          end
+          i += 1
+        end
+        return array  
       end
 
       def search(_array, _query)
-        0
+        left = 0
+        right = _array.length - 1 
+        while right >= left
+          middle = (left + right) / 2
+          if _array[middle] == _query
+            return middle
+          elsif _array[middle] < _query
+            left = middle + 1
+          else 
+            right = middle - 1
+          end
+        end
+        return -1
       end
     end
   end
