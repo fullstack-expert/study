b7bd70fd0203d916718e43a9a3c1b1bf6639d891
Tatiana Kalmykova <ul.tkalmykova@gmail.com>
2020-04-16 10:02:40 +0400

    implement recursive binary_search

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -11,25 +11,21 @@ module Exercise
       def search(array, query)
         return -1 if array.empty?
 
-        if array.length == 1
-          return array[0] == query ? 0 : -1
-        end
-
-        middle = array.length / 2
-        start_index = 0
-        end_index = array.length - 1
-        while start_index <= end_index
-          middle = (start_index + end_index) / 2
-
-          if array[middle] < query
-            start_index = middle + 1
-          elsif array[middle] > query
-            end_index = middle - 1
-          else
-            return middle
+        binary_search = lambda do |start_index, end_index|
+          if start_index == end_index
+            return start_index if array[start_index] == query
+            return -1
           end
+
+          middle = (start_index + end_index) / 2
+          return middle if array[middle] == query
+
+          return binary_search.call(middle + 1, end_index) if array[middle] < query
+
+          binary_search.call(start_index, middle - 1)
         end
-        -1
+
+        binary_search.call(0, array.length - 1)
       end
 
       def max_element(array)
