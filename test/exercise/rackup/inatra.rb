5f2374e9fe4880cb8f54c17d907339949af11b1f
Vasilek <morphizm@ya.ru>
2020-04-17 08:28:14 +0300

    instance eval

--- test/exercise/rackup/inatra.rb
+++ test/exercise/rackup/inatra.rb
@@ -1,20 +1,11 @@
 module Inatra
   class << self
     def routes
-      {
-        'GET' => {
-          '/hello' => ->() { hello }
-        },
-        'POST' => {
-          '/tasks' => ->() { tasks }
-        }
-      }
     end
 
     def call(env)
-      request_method = env['REQUEST_METHOD']
       path = env['PATH_INFO']
-      # routes[request_method][path].call
+      instance_eval path.slice(1, path.length)
     end
 
     def hello
@@ -23,11 +14,6 @@ module Inatra
 
     def tasks
       [201, {}, 'Task created']
-    end
-
-    def method_missing(m, *args, &block)
-      p "Delegating #{m}"
-      # object.send(m, *args, &block)
     end
   end
 end
