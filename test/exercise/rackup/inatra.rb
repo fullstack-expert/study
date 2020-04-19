a5e27cd7826b14ea1e09b1bb1e27ee9acfbd5d45
Vasilek <morphizm@ya.ru>
2020-04-19 16:30:59 +0300

    get tasks

--- test/exercise/rackup/inatra.rb
+++ test/exercise/rackup/inatra.rb
@@ -5,44 +5,20 @@ module Inatra
 
     def call(env)
       path = env['PATH_INFO']
-      instance_eval path.slice(1, path.length)
+      method = env['REQUEST_METHOD']
+      instance_eval "#{method.downcase}_#{path.slice(1, path.length)}"
     end
 
-    def hello
+    def get_hello
       [200, {}, 'Hello World']
     end
 
-    def tasks
+    def post_tasks
       [201, {}, 'Task created']
     end
 
-    def method_missing(m, *args, &block)
-      p "Delegating #{m}"
-      # object.send(m, *args, &block)
-    def call(_env)
-      headers = {}
-      mapping = {
-        'GET' => {
-          '/hello' => ->() { hello }
-        },
-        'POST' => {
-          '/tasks' => ->() { tasks }
-        }
-      }
-    end
-
-    def call(env)
-      request_method = env['REQUEST_METHOD']
-      path = env['PATH_INFO']
-      routes[request_method][path].call
-    end
-
-    def hello
-      [200, {}, 'Hello World']
-    end
-
-    def tasks
-      [201, {}, 'Task created']
+    def get_tasks
+      [200, {}, 'Task list']
     end
   end
 end
