71131aa4ae79323f76d2a2bb74665b97c3fc7e48
d.glebov <d.glebov@gmail.com>
2020-04-21 15:29:01 +0300

    5th attempt

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,8 +4,7 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        array = _array
+      def rating(array)
         newtable = array.select { |row| 
                               row["rating_kinopoisk"].to_f > 0 &&  
                               row["country"] != nil &&
@@ -13,12 +12,10 @@ module Exercise
         newtable.reduce(0) { |sum, row| sum + row["rating_kinopoisk"].to_f} / newtable.count           
       end
 
-      def chars_count(_films, _threshold)
-        films, threshold = _films, _threshold
+      def chars_count(films, threshold)
         films.select {|row| 
                           row['rating_kinopoisk'].to_f > threshold}.reduce(0) { |sum, row|  
                           sum + row['name'].count('и')}
-
       end
     end
   end
