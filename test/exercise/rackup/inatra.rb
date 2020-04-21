09e20861a988b3234319834f243238246895b221
Tatiana Kalmykova <ul.tkalmykova@gmail.com>
2020-04-21 08:32:08 +0400

    implement Inatra

--- test/exercise/rackup/inatra.rb
+++ test/exercise/rackup/inatra.rb
@@ -1,9 +1,25 @@
+require 'byebug'
+
 module Inatra
   class << self
-    def routes
+    def routes(&block)
+      instance_eval &block
     end
 
     def call(env)
+      handle_request(env['REQUEST_METHOD'], env['PATH_INFO'])
+    end
+
+    private
+
+    def method_missing(name, path, &block)
+      define_method "#{name}_#{path}" do
+        instance_eval &block
+      end
+    end
+
+    def handle_request(method, path)
+      send("#{method}_#{path}")
     end
   end
 end
