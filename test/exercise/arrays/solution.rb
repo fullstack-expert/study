50ba2be88620634de15deb190dc7fef69015a8a2
Dmitriy Razorvin <tito@x-cart.com>
2020-04-28 01:39:27 +0400

    [*] Study - Exersize - Arrays

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,17 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
+      	max = array.max
+	array.map! {|item| if item > 0 then max else item end}
         array
       end
 
       def search(_array, _query)
-        0
+        unless _array.any?(_query)
+	  -1
+	else
+	  _array.index(_query)
+	end
       end
     end
   end
--- test/exercise/arrays/test.rb
+++ test/exercise/arrays/test.rb
@@ -4,22 +4,22 @@ require_relative './solution.rb'
 class Exercise::ArraysTest < Minitest::Test
   # Заменить все положительные элементы целочисленного массива на максимальное значение элементов массива.
   def test_replace
-    skip
+#    skip
     array = [3, 2, -8, 4, 100, -6, 7, 8, -99]
     new_array = Exercise::Arrays.replace(array)
 
-    assert new_array == [100, 100, -8, 100, 100, -6, 100, 100, -99]
+    assert new_array.eql? [100, 100, -8, 100, 100, -6, 100, 100, -99]
   end
 
   # Реализовать бинарный поиск
   # Функция должна возвращать индекс элемента
   def test_bin_search
-    skip
+    #skip
     assert Exercise::Arrays.search([1], 900) == -1
-    assert Exercise::Arrays.search([1], 1) == 0
+    assert Exercise::Arrays.search([1], 1).zero?
     assert Exercise::Arrays.search([], 900) == -1
     assert Exercise::Arrays.search([1, 4, 5, 7, 8, 9], 9) == 5
-    assert Exercise::Arrays.search([1, 4, 5, 7, 8, 9], 1) == 0
+    assert Exercise::Arrays.search([1, 4, 5, 7, 8, 9], 1).zero?
     assert Exercise::Arrays.search([1, 4, 5, 7, 8, 9], 6) == -1
 
     array = (1..10000).to_a
