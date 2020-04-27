349dfbbeefd32ceec07821172ae2c0e80d426126
Dmitriy Razorvin <tito@x-cart.com>
2020-04-28 03:17:12 +0400

    Study - Exersize - Fp

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,11 +5,34 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(_array)
-        0
+      	_array = _array.map {|film| 
+		if !film['country'].nil? && 
+		   film['country'].split(',').count > 1 && 
+		   !film["rating_kinopoisk"].nil? && 
+		   film["rating_kinopoisk"].to_f > 0 
+		then 
+		   film["rating_kinopoisk"].to_f
+		else 
+		   0 
+		end
+	}
+	_array.delete(0)
+        totalRating = _array.reduce {|sum, film_rating| sum + film_rating}
+	totalRating / _array.count.to_f
       end
 
       def chars_count(_films, _threshold)
-        0
+      	_films = _films.map {|film| 
+		if !film["rating_kinopoisk"].nil? && 
+		   film["rating_kinopoisk"].to_f >= _threshold 
+		then 
+		   film["name"]
+		else 
+		   '' 
+		end
+	}
+	_films.delete('')
+        _films.to_s.count('и')
       end
     end
   end
