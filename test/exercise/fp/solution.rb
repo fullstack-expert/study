c0eddfe0b83994a5922476f19f1fbfc157f1273f
Andrej Durasow <smailik2402@gmail.com>
2020-04-29 13:05:31 +0400

    fixed fp

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,16 +5,16 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(array)
-        reit = array.filter_map { |film| film if film["country"].present? }.
+        rate = array.filter_map { |film| film if film["country"].present? }.
                      filter_map { |film| film if film["rating_kinopoisk"].present? }.
                      filter_map { |film| film if film["rating_kinopoisk"].to_f > 0 }.
                      filter_map { |film| film["rating_kinopoisk"].to_f if film["country"].split(',').length >= 2 }
-        result = reit.reduce(0, :+) { |res| res.to_f }
-        result / reit.length
+        result = rate.reduce(0, :+) { |res| res.to_f }
+        result / rate.length
       end
 
       def chars_count(films, threshold)
-        res = films.filter_map { |film| film unless film["rating_kinopoisk"].nil? }.
+        res = films.filter_map { |film| film if film["rating_kinopoisk"].present? }.
               filter_map { |film| film["name"] if film["rating_kinopoisk"].to_f >= threshold }.
               map { |name| name.count('и') }
         res.reduce(0, :+) { |name| name }
