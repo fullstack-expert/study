7a25ae048b162807825e47bdc7643487f2326f20
Andrej Durasow <smailik2402@gmail.com>
2020-05-05 12:43:19 +0400

    fp fixed by select and map

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,19 +5,16 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(array)
-        rate = array.filter_map { |film| film if film["country"].present? }.
-                     filter_map { |film| film if film["rating_kinopoisk"].present? }.
-                     filter_map { |film| film if film["rating_kinopoisk"].to_f > 0 }.
-                     filter_map { |film| film["rating_kinopoisk"].to_f if film["country"].split(',').length >= 2 }
-        result = rate.reduce(0, :+) { |res| res.to_f }
-        result / rate.length
+        rate = array.select { |film| film['country'].to_s.include?(',') &&
+                     film["rating_kinopoisk"].present? &&
+                     film["rating_kinopoisk"].to_f > 0 }
+        result = rate.map { |film| film["rating_kinopoisk"].to_f}
+        result.reduce(:+).to_f / rate.length
       end
 
       def chars_count(films, threshold)
-        res = films.filter_map { |film| film if film["rating_kinopoisk"].present? }.
-              filter_map { |film| film["name"] if film["rating_kinopoisk"].to_f >= threshold }.
-              map { |name| name.count('и') }
-        res.reduce(0, :+) { |name| name }
+        res = films.select { |film| film['rating_kinopoisk'].to_f >= threshold }              
+        res.map { |film| film['name'].count 'и' }.reduce(:+)
       end
     end
   end
