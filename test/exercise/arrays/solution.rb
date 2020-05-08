79d8239e138bf82507c041aba7642f00c0a5ff0d
Ly1ngCat <waltersullivan293@gmail.com>
2020-05-08 18:21:14 +0400

    Arrays

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -1,12 +1,32 @@
 module Exercise
   module Arrays
     class << self
+      
       def replace(array)
-        array
+        max = array.reduce() { |max, el| max < el ? el : max }
+        array.map { |i| i > 0 ? max : i }
       end
 
       def search(_array, _query)
-        0
+      
+        firstIndex = 0
+        lastIndex = _array.length - 1
+        mid = (firstIndex + lastIndex) / 2
+
+        while firstIndex <= lastIndex
+          if _array[mid] == _query
+            return  mid
+          elsif _array[mid] < _query
+            firstIndex = mid + 1
+            mid = (firstIndex + lastIndex) / 2
+          elsif _array[mid] > _query
+            lastIndex = mid - 1
+            mid = (firstIndex + lastIndex) / 2 
+          end
+        end
+        
+        return -1 
+
       end
     end
   end
