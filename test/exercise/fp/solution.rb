21b664aabf00647215c0941459d39fc7441c9078
Ly1ngCat <waltersullivan293@gmail.com>
2020-05-08 21:23:14 +0400

    Rubocop style fixes

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -1,20 +1,19 @@
 module Exercise
   module Fp
     class << self
-
       def rating(_array)
-        filmSelect = array.select { |film| film["country"].to_s.include?(",") && !film["rating_kinopoisk"].nil? && film["rating_kinopoisk"].to_f > 0 }
-        
+        filmSelect = array.select { |film| film['country'].to_s.include?(',') && !film['rating_kinopoisk'].nil? && film['rating_kinopoisk'].to_f > 0 }
+
         raitingNumber = raitings.lenght
-        raitings = filmSelect.map { |film| film["raiting_kinopoisk"].to_f }
+        raitings = filmSelect.map { |film| film['raiting_kinopoisk'].to_f }
         raitingSum = raitings.reduce(:+)
-        return raitingSum / raitingNumber
+        raitingSum / raitingNumber
       end
 
       def chars_count(_films, _threshold)
-        filmSelect = films.select { |film| film ["raiting_kinopoisk"].to_f >= threshold }
-        filmNames = filmSelect.map { |film| film["name"].to_s}
-        return filmNames.map { |name| name.cout("и") }.reduce(:+)
+        filmSelect = films.select { |film| film ['raiting_kinopoisk'].to_f >= threshold }
+        filmNames = filmSelect.map { |film| film['name'].to_s }
+        filmNames.map { |name| name.cout('и') }.reduce(:+)
       end
     end
   end
