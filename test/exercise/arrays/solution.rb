9b8ce75473adefb40af0960bda4bdbbbdebe3834
Aleksandr Pushin <pushinap@>
2020-08-05 01:04:19 +0300

    Add solution for array

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,45 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max_number = search_max_number(array)
+        new_array = []
+
+        array.each do |number|
+          if number.positive?
+            new_array.push(max_number)
+          else
+            new_array.push(number)
+          end
+        end
+        new_array
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        return -1 if array.empty?
+
+        bsearch = lambda { |array2, index|
+          if array2.length == 1
+            return array2.first == query ? index : -1
+          end
+
+          if query <= array2[array2.length / 2 - 1]
+            bsearch.call(array2[0, array2.length / 2], index)
+          else
+            bsearch.call(array2[array2.length / 2, array2.length], index + array2.length / 2)
+          end
+        }
+        bsearch.call(array, 0)
+      end
+
+      private
+
+      def search_max_number(numbers)
+        max_number = 0
+
+        numbers.each do |number|
+          max_number = number if number > max_number
+        end
+        max_number
       end
     end
   end
