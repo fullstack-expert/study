89c17cfa1afd58d39d7c3547bd23c4712e4f900b
Aleksandr Pushin <pushinap@>
2020-08-07 16:48:38 +0300

    Add solution for Inatra

--- test/exercise/rackup/inatra.rb
+++ test/exercise/rackup/inatra.rb
@@ -1,7 +1,30 @@
 module Inatra
   class << self
-    def routes(&block); end
+    WHITELIST_REQUEST_METHODS = %i[get post put patch delete].freeze
 
-    def call(env); end
+    def routes(&block)
+      module_eval(&block)
+    end
+
+    def call(env)
+      request_method = env['REQUEST_METHOD'].downcase.to_sym
+      request_path = env['PATH_INFO']
+
+      @routes[request_method].key?(request_path) ? @routes[request_method][request_path] : [404, {}, ['Not Found']]
+    end
+
+    def method_missing(request_method, path)
+      if WHITELIST_REQUEST_METHODS.include?(request_method)
+        @routes ||= {}
+        @routes[request_method] ||= {}
+        @routes[request_method][path] = yield
+      else
+        super
+      end
+    end
+
+    def respond_to_missing?(request_method)
+      WHITELIST_REQUEST_METHODS.include?(request_method)
+    end
   end
 end
--- test/exercise/rackup/test.rb
+++ test/exercise/rackup/test.rb
@@ -6,38 +6,30 @@ require_relative './my_app'
 
 class RackTest < Test::Unit::TestCase
   def test_it_says_hello_world
-    omit do
-      browser = Rack::Test::Session.new(Rack::MockSession.new(Inatra))
-      browser.get '/hello'
-      assert browser.last_response.ok?
-      assert_equal 'Hello World', browser.last_response.body
-    end
+    browser = Rack::Test::Session.new(Rack::MockSession.new(Inatra))
+    browser.get '/hello'
+    assert browser.last_response.ok?
+    assert_equal 'Hello World', browser.last_response.body
   end
 
   def test_it_pongs
-    omit do
-      browser = Rack::Test::Session.new(Rack::MockSession.new(Inatra))
-      browser.get '/ping'
-      assert browser.last_response.ok?
-      assert_equal 'PONG', browser.last_response.body
-    end
+    browser = Rack::Test::Session.new(Rack::MockSession.new(Inatra))
+    browser.get '/ping'
+    assert browser.last_response.ok?
+    assert_equal 'PONG', browser.last_response.body
   end
 
   def test_it_says_bye
-    omit do
-      browser = Rack::Test::Session.new(Rack::MockSession.new(Inatra))
-      browser.post '/bye'
-      assert browser.last_response.ok?
-      assert_equal 'Bye Bye', browser.last_response.body
-    end
+    browser = Rack::Test::Session.new(Rack::MockSession.new(Inatra))
+    browser.post '/bye'
+    assert browser.last_response.ok?
+    assert_equal 'Bye Bye', browser.last_response.body
   end
 
   def test_it_handles_404
-    omit do
-      browser = Rack::Test::Session.new(Rack::MockSession.new(Inatra))
-      browser.get '/missing_method'
-      assert browser.last_response.not_found?
-      assert_equal 'Not Found', browser.last_response.body
-    end
+    browser = Rack::Test::Session.new(Rack::MockSession.new(Inatra))
+    browser.get '/missing_method'
+    assert browser.last_response.not_found?
+    assert_equal 'Not Found', browser.last_response.body
   end
 end
