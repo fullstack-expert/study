50d8c4624eae5b3dd4201d93dd3c7e1467e45206
Aleksandr Pushin <pushinap@>
2020-08-07 16:42:23 +0300

    Add solution for FP2

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,16 +5,40 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each; end
+      def my_each(&block)
+        each = lambda { |array|
+          return self if array.my_empty?
+
+          block.call(array[0])
+          each.call(array[1, array.length])
+        }
+        each.call(self)
+      end
 
       # Написать свою функцию my_map
-      def my_map; end
+      def my_map(&block)
+        func = ->(acc, element) { acc << block.call(element) }
+
+        my_reduce(MyArray.new, &func)
+      end
 
       # Написать свою функцию my_compact
-      def my_compact; end
+      def my_compact
+        func = ->(acc, element) { element.nil? ? acc : acc << element }
+
+        my_reduce(MyArray.new, &func)
+      end
 
       # Написать свою функцию my_reduce
-      def my_reduce; end
+      def my_reduce(acc = nil, &block)
+        my_each { |element| acc = acc.nil? ? element : block.call(acc, element) }
+
+        acc
+      end
+
+      def my_empty?
+        length == 0
+      end
     end
   end
 end
--- test/exercise/fp2/test.rb
+++ test/exercise/fp2/test.rb
@@ -11,7 +11,6 @@ class Exercise::Fp2Test < Minitest::Test
   end
 
   def test_my_each
-    skip
     result = []
     my_result = []
 
@@ -23,24 +22,23 @@ class Exercise::Fp2Test < Minitest::Test
   end
 
   def test_my_map
-    skip
     func = ->(element) { element * @int }
+
     assert @array.map(&func) == @my_array.my_map(&func)
     assert @array.map(&func).map(&func) == @my_array.my_map(&func).my_map(&func)
   end
 
   def test_my_compact
-    skip
     func = ->(element) { element if element.even? }
     func_another = ->(element) { element * @int }
     func_yet_another = ->(element) { element.even? }
+
     assert @array.map(&func).compact == @my_array.my_map(&func).my_compact
     assert @array.map(&func).compact.map(&func_another) == @my_array.my_map(&func).my_compact.my_map(&func_another)
     assert @array.map(&func_yet_another).compact == @my_array.my_map(&func_yet_another).my_compact
   end
 
   def test_my_reduce
-    skip
     func = ->(acc, element) { acc * element }
 
     assert @array.reduce(&func) == @my_array.my_reduce(&func)
