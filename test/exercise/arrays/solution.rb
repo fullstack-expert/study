dd972322d303cfa612e75f9e8e691db7f701ef35
Anastasia <anastasia.zhura@gmail.com>
2020-08-23 13:47:40 +0300

    complete array

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,28 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        largest = array.inject do |memo, num|
+          memo > num ? memo : num
+        end
+
+        array.map { |el| el.positive? ? largest : el }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        low = 0
+        high = array.length - 1
+        while low <= high
+          mid = (low + high)
+          guess = array[mid]
+          if guess == query
+            return mid
+          elsif guess > query
+            high = mid - 1
+          else
+            low = mid + 1
+          end
+        end
+        -1
       end
     end
   end
