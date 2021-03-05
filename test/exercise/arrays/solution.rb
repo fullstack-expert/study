7e5fb5a3da65489e8799a1d17d1a0e753adc0c29
paulreedsmith <forall@mm.st>
2021-03-05 22:29:53 +0300

    add replace method at arrays exercise

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,7 +2,7 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        array.map {|number| (number.positive? && number != array.max) ? array.max : number}
       end
 
       def search(_array, _query)
