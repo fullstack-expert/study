28351a9063a882dc4284390e4511abebbdbf58e9
paulreedsmith <forall@mm.st>
2021-03-09 00:39:11 +0300

    add search method at arrays exercise

--- Makefile
+++ Makefile
@@ -2,6 +2,9 @@ build:
 	docker-compose build
 	docker-compose run --rm ruby bash -c 'bundle install'
 
+lint:
+	docker-compose run --rm ruby bash -c 'bundle exec rubocop'
+
 test:
 	docker-compose run --rm ruby bash -c 'bundle exec rake test'
 
--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,31 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array.map {|number| (number.positive? && number != array.max) ? array.max : number}
+        max = array.max
+        array.map { |number| number.positive? ? max : number }
       end
 
-      def search(_array, _query)
-        0
+      def bin_search(arr, low, high, query)
+        return -1 if low > high
+
+        mid = (low + high) / 2
+
+        if query == arr[mid]
+          mid
+        elsif query < arr[mid]
+          bin_search(arr, low, mid - 1, query)
+        else
+          bin_search(arr, mid + 1, high, query)
+        end
+      end
+
+      def search(array, query)
+        return -1 if !array || !query
+        return -1 if array.empty?
+
+        low = array.index(array[0])
+        high = array.index(array.max)
+        bin_search(array, low, high, query)
       end
     end
   end
