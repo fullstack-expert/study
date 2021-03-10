478d43df31319b083147a6c34dc164bdd4b82885
Alexey Solovyev <alex.solovyev@gmail.com>
2021-03-11 01:44:02 +0400

    replace & search methods implemented

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,26 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max = array.max
+        array.map { |v| v.positive? ? max : v }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        left = 0
+        right = array.length - 1
+        while left <= right
+          middle = ((left + right) / 2).floor
+
+          return middle if array[middle] == query
+
+          if array[middle] < query
+            left = middle + 1
+          else
+            right = middle - 1
+          end
+        end
+
+        -1
       end
     end
   end
