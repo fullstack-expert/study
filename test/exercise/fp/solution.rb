4a4d6785367b9b6782dfaa83091e8c7b83e05c8b
paulreedsmith <forall@mm.st>
2021-03-10 03:30:19 +0300

    add rating method at fp exercise

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,10 +4,18 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        filtred = array.select do |film|
+          film['country'].to_s.split(',').length > 1 && film['rating_kinopoisk'].to_f.positive?
+        end
+        maped = filtred.map { |film| film['rating_kinopoisk'].to_f }
+        sum = maped.reduce(:+)
+        count = filtred.length
+        sum / count
       end
 
+      # Посчитать количесвто букв 'и' в названиях всех фильмов
+      # с рейтингом кинопоиска больше или равным заданному значению
       def chars_count(_films, _threshold)
         0
       end
