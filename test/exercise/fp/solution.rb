b0819d8f75aa87a151ac7eec6fdcc6c52bc872f3
paulreedsmith <forall@mm.st>
2021-03-11 02:50:17 +0300

    typo fix at fp exercise

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,21 +5,21 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(array)
-        filtred = array.select do |film|
+        filtered = array.select do |film|
           film['country'].to_s.split(',').length > 1 && film['rating_kinopoisk'].to_f.positive?
         end
-        maped = filtred.map { |film| film['rating_kinopoisk'].to_f }
-        sum = maped.reduce(:+)
-        count = filtred.length
+        mapped = filtered.map { |film| film['rating_kinopoisk'].to_f }
+        sum = mapped.reduce(:+)
+        count = filtered.length
         sum / count
       end
 
       # Посчитать количесвто букв 'и' в названиях всех фильмов
       # с рейтингом кинопоиска больше или равным заданному значению
       def chars_count(films, threshold)
-        filtred = films.select { |film| film['rating_kinopoisk'].to_f >= threshold }
-        maped = filtred.map { |film| film['name'].to_s }
-        maped.reduce(0) { |acc, name| acc + name.count('и') }
+        filtered = films.select { |film| film['rating_kinopoisk'].to_f >= threshold }
+        mapped = filtered.map { |film| film['name'].to_s }
+        mapped.reduce(0) { |acc, name| acc + name.count('и') }
       end
     end
   end
