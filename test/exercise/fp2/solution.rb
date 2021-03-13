beddcb2b1ae7b67c56ddc8966a3a66408c39928f
paulreedsmith <forall@mm.st>
2021-03-13 23:20:49 +0300

    add methods at fp2 exercise

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,16 +5,38 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each; end
+      def my_each(&func)
+        array = self
+        return array if array.empty?
+
+        head, *tail = array
+
+        yield head
+
+        MyArray.new(tail).my_each(&func)
+        array
+      end
 
       # Написать свою функцию my_map
-      def my_map; end
+      def my_map
+        func = ->(acc, el) { acc << yield(el) }
+        empty_arr = MyArray.new
+        my_reduce(empty_arr, &func)
+      end
 
       # Написать свою функцию my_compact
-      def my_compact; end
+      def my_compact
+        func = ->(acc, el) { el.nil? ? acc : acc << el }
+        empty_arr = MyArray.new
+        my_reduce(empty_arr, &func)
+      end
 
       # Написать свою функцию my_reduce
-      def my_reduce; end
+      def my_reduce(acc = nil)
+        arr = self
+        my_each { |el| acc.nil? ? acc = arr.first : acc = yield(acc, el) }
+        acc
+      end
     end
   end
 end
