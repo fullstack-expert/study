022f8ef083456a5a51439b39aa12ab90c5aba362
sergey.mishin <s.mishin@skyeng.ru>
2021-03-15 01:52:13 +0300

    Solve #1 - arrays

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,26 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max_el = array.inject(array[0]) { |el, acc| el > acc ? el : acc }
+        array.map { |el| el.positive? ? max_el : el }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        iter = lambda do |part, from|
+          return -1 if part.empty?
+
+          mid = part.length / 2
+          guess = part[mid]
+          return from + mid if guess == query
+
+          if query < guess
+            iter.call(part[0..(mid - 1)], from)
+          else
+            iter.call(part[(mid + 1)..], from + mid + 1)
+          end
+        end
+
+        iter.call(array, 0)
       end
     end
   end
