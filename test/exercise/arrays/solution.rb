5756c1d9cec823cf7440f804bde40b781b9db56f
krivtsov <krivtsov.su@gmail.com>
2021-04-12 23:51:30 +0400

    add resolve exercise arrays

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,30 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max = array.reduce { |largest, current| largest > current ? largest : current }
+        array.map { |n| n.positive? ? max : n }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        if array.empty?
+          -1
+        else
+          start = 0
+          finish = array.length - 1
+          iter(start, finish, array, query)
+        end
+      end
+
+      def iter(start, finish, array, query)
+        middle = (start + finish) / 2
+
+        return middle if array[middle] == query
+
+        return -1 if start == finish
+
+        return iter(middle + 1, finish, array, query) if array[middle] < query
+
+        iter(start, middle - 1, array, query)
       end
     end
   end
