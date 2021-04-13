5b2d03274e924a1fe340fd036bfb351c018fd428
Artyom V. Peredery <artperedery@gmail.com>
2021-04-14 00:58:23 +0300

    feat: search & replace methods for Array

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,27 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max = find_max(array)
+        result = []
+        array.each { |e| result.push e >= 0 ? max : e }
+        result
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query, low = 0, hight = array.length - 1)
+        return -1 if low > hight || query < array[low] || query > array[hight]
+
+        mid = low + (hight - low) / 2
+        return mid if array[mid] == query
+
+        array[mid] > query ? search(array, query, low, mid - 1) : search(array, query, mid + 1, hight)
+      end
+
+      def find_max(array)
+        max = -Float::INFINITY
+        array.each do |e|
+          max = e if e >= max
+        end
+        max
       end
     end
   end
