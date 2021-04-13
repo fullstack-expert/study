8c2e797e74595a43d982a71526ea28ba0392163f
Nataly Blum <nblyumenshteyn@gmail.com>
2021-04-13 22:49:51 +0400

    test arrays

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,38 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        new_max = -1
+        array.each { |val| new_max = val if val > new_max }
+        array.map { |val| val.positive? ? new_max : val }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        recurs = lambda { |arr, target, min_index, max_index|
+          if arr.length.positive?
+            mid_index = ((min_index + max_index).to_i / 2).to_i
+            case arr[mid_index].to_i <=> target
+            when 0
+              mid_index
+            when -1
+              if mid_index == max_index
+                -1
+              else
+                min_index = mid_index + 1
+                recurs.call(arr, target, min_index, max_index)
+              end
+            when  1
+              if mid_index == min_index
+                -1
+              else
+                max_index = mid_index - 1
+                recurs.call(arr, target, min_index, max_index)
+              end
+            end
+          else
+            -1
+          end
+        }
+        recurs.call(array, query, 0, array.size - 1)
       end
     end
   end
--- test/exercise/arrays/test.rb
+++ test/exercise/arrays/test.rb
@@ -4,24 +4,20 @@ require_relative './solution.rb'
 class Exercise::ArraysTest < Minitest::Test
   # Заменить все положительные элементы целочисленного массива на максимальное значение элементов массива.
   def test_replace
-    skip
     array = [3, 2, -8, 4, 100, -6, 7, 8, -99]
     new_array = Exercise::Arrays.replace(array)
-
     assert new_array == [100, 100, -8, 100, 100, -6, 100, 100, -99]
   end
 
   # Реализовать двоичный поиск
   # Функция должна возвращать индекс элемента
   def test_bin_search
-    skip
     assert Exercise::Arrays.search([1], 900) == -1
     assert Exercise::Arrays.search([1], 1).zero?
     assert Exercise::Arrays.search([], 900) == -1
     assert Exercise::Arrays.search([1, 4, 5, 7, 8, 9], 9) == 5
     assert Exercise::Arrays.search([1, 4, 5, 7, 8, 9], 1).zero?
     assert Exercise::Arrays.search([1, 4, 5, 7, 8, 9], 6) == -1
-
     array = (1..10000).to_a
     assert Exercise::Arrays.search(array, array[1000]) == 1000
   end
