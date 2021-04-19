9ef43f9faf3c9f2937f9f8e5151ff520692232a1
Nataly Blum <nblyumenshteyn@gmail.com>
2021-04-20 01:17:04 +0400

    fix arrays2

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -3,20 +3,20 @@ module Exercise
     class << self
       def find_max(array)
         new_max = array[0]
-        arr.each { |element| new_max = element if element > new_max }
+        array.each { |element| new_max = element if element > new_max }
         new_max
       end
 
       def replace(array)
-        return unless array.empty?
+        return if array.empty?
 
         new_max = find_max(array)
-        array.map { |val| val.empty? ? new_max : val }
+        array.map { |val| val.positive? ? new_max : val }
       end
 
       def search(array, query)
         recurs = lambda { |arr, target, min_index, max_index|
-          if arr.empty?
+          if !arr.empty?
             mid_index = ((min_index + max_index).to_i / 2).to_i
             case arr[mid_index].to_i <=> target
             when 0
