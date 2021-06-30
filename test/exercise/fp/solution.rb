810f6b84688a4b2d1bd447e57f5f041d34473839
Oleg Nayanov <oleg.nayanov@dualbootpartners.com>
2021-06-30 22:56:08 +0400

    added solutions

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,12 +4,19 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        selected_films = array.reject { |film| film['rating_kinopoisk'].to_f < 2 || film['country'].nil? }
+                              .select { |film| film['country'].split(',').size >= 2 }
+                              .map { |film| film['rating_kinopoisk'].to_f }
+
+        selected_films.reduce(:+) / selected_films.size
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def chars_count(films, threshold)
+        films.reject { |film| film['rating_kinopoisk'].to_f < threshold || !film['name'].include?('и') }
+             .map { |film| film['name'] }
+             .map { |name| name.split('').select { |word| word == 'и' }.size }
+             .reduce(:+)
       end
     end
   end
