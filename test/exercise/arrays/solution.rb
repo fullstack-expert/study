913424b851f5a87639cd5e5cc093dc9fc4819419
Leonid Chirkov <leonidchirkovul@gmail.com>
2021-07-25 13:09:13 +0400

    add solution for arrays

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,23 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        maximum = array.reduce { |acc, el| acc > el ? acc : el }
+        array.map { |el| el.positive? ? maximum : el }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        index = (array.length - 1) / 2
+
+        return -1 if array.length.zero? || (array.length == 1 && array[index] != query)
+
+        if query == array[index]
+          index
+        elsif query > array[index]
+          acc = search(array[index + 1, array.length], query)
+          acc == -1 ? -1 : index + 1 + acc
+        else
+          search(array[0, index], query)
+        end
       end
     end
   end
