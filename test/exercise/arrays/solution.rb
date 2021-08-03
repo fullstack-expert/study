8132bfe17ff2b70ff96af2095ad65ab66aad136c
effgenij <effgenij@gmail.com>
2021-08-03 16:19:54 +0300

    Added arrays solution

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,29 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max_number = get_max_number(array)
+        array.map { |num| num.positive? ? max_number : num }
       end
 
-      def search(_array, _query)
-        0
+      def get_max_number(array)
+        max_number = array[0]
+        array.each { |num| max_number = num > max_number ? num : max_number }
+        max_number
+      end
+
+      def search(array, query)
+        iter(0, array.length - 1, array, query)
+      end
+
+      def iter(left, right, array, query)
+        mid_index = (left + right) / 2
+        number = array[mid_index]
+        return mid_index if number == query
+
+        return -1 if left >= right
+        return iter(left, mid_index, array, query) if number > query
+
+        iter(mid_index + 1, right, array, query)
       end
     end
   end
