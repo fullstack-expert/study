fc45bbbfed2ca5b073658e24303c6485536aa218
feycot <feycot@gmail.com>
2021-08-12 20:28:13 +0000

    add solution replace

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,51 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        replacer = max(array)
+        last_index = array.size - 1
+
+        iter = -> (arr, current_index) {
+          current = arr[current_index]
+
+          arr[current_index] = current.positive? ? replacer : current
+
+          return  current_index == last_index ? arr : iter.call(arr, current_index + 1)
+        }
+
+        iter.call(array, 0)
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        return -1 if array.empty?
+
+        low = 0
+        high = array.size - 1
+
+        while low <= high
+          mid = (low + high).ceil
+          if array[mid] == query
+            return mid
+          end
+
+          if query < array[mid]
+            high = mid - 1
+          else
+            low = mid + 1
+          end
+        end
+
+        return -1
+      end
+
+      def max(array)
+        iter = -> (arr, acc) {
+          return acc if arr.empty?
+          tmp = arr.first
+          newAcc = acc > tmp ? acc : tmp
+          return iter.call(arr[1..], newAcc)
+        }
+
+        iter.call(array, array.first)
       end
     end
   end
