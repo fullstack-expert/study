b2f7413e6a5c9e9a4ba823e0adf9f40a6cf21797
feycot <feycot@gmail.com>
2021-08-14 16:33:29 +0500

    add solution

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -5,46 +5,50 @@ module Exercise
         replacer = max(array)
         last_index = array.size - 1
 
-        iter = -> (arr, current_index) {
+        iter = lambda do |arr, current_index|
           current = arr[current_index]
 
           arr[current_index] = current.positive? ? replacer : current
 
-          return  current_index == last_index ? arr : iter.call(arr, current_index + 1)
-        }
+          return current_index == last_index ? arr : iter.call(arr, current_index + 1)
+        end
 
         iter.call(array, 0)
       end
 
       def search(array, query)
-        return -1 if array.empty?
+        iter = lambda do |low, high|
+          return -1 if high < low
+
+          guess = (low + high) / 2
+
+          case array[guess] <=> query
+          when 0
+            guess
+          when -1
+            low = guess + 1
+            iter.call(low, high)
+          when 1
+            high = guess - 1
+            iter.call(low, high)
+          end
+        end
 
         low = 0
         high = array.size - 1
 
-        while low <= high
-          mid = (low + high).ceil
-          if array[mid] == query
-            return mid
-          end
-
-          if query < array[mid]
-            high = mid - 1
-          else
-            low = mid + 1
-          end
-        end
-
-        return -1
+        iter.call(low, high)
       end
 
       def max(array)
-        iter = -> (arr, acc) {
+        iter = lambda do |arr, acc|
           return acc if arr.empty?
+
           tmp = arr.first
-          newAcc = acc > tmp ? acc : tmp
-          return iter.call(arr[1..], newAcc)
-        }
+          new_acc = acc > tmp ? acc : tmp
+
+          return iter.call(arr.slice(1, arr.size - 1), new_acc)
+        end
 
         iter.call(array, array.first)
       end
