9d3a3d3a98f96c84226b3387f8087928ce6613e4
feycot <feycot@gmail.com>
2021-08-14 21:33:12 +0500

    add fp2 solution

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,16 +5,53 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each; end
+      def my_each
+
+        for el in self do
+          yield el
+        end
+
+        self.class.new self
+      end
 
       # Написать свою функцию my_map
-      def my_map; end
+      def my_map
+        result = []
+        for el in self do
+          new_el = yield el
+          result << new_el
+        end
+
+        self.class.new result
+      end
 
       # Написать свою функцию my_compact
-      def my_compact; end
+      def my_compact
+        result = []
+        for el in self do
+          if block_given?
+            new_el = yield el
+            result << new_el
+          else
+            result << el unless el.nil?
+          end
+        end
+
+        self.class.new result
+      end
 
       # Написать свою функцию my_reduce
-      def my_reduce; end
+      def my_reduce(initial = nil)
+        acc = initial.nil? ?  self[0] : initial
+
+        items = initial.nil? ? self[1..] : self
+
+        for el in items do
+          acc = yield acc, el
+        end
+
+        acc
+      end
     end
   end
 end
