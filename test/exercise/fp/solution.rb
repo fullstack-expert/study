4d7279d9737b6e54ca2c8d4f7602337352ec6721
feycot <feycot@gmail.com>
2021-08-14 19:55:40 +0500

    add chars_count solution

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -6,22 +6,23 @@ module Exercise
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(films)
         films_with_two_or_more_countries = films.select do |film|
-          count_film_countries = film["country"].to_s.split(',').size
+          count_film_countries = film['country'].to_s.split(',').size
 
           count_film_countries > 1
         end
 
-        ratings = films_with_two_or_more_countries
-          .map {|film| film["rating_kinopoisk"].to_f}
-          .select {|rating| rating > 0}
+        ratings = films_with_two_or_more_countries.map { |film| film['rating_kinopoisk'].to_f }.select(&:positive?)
 
-        sum_of_ratings = ratings.reduce(0) {|acc, rating| acc + rating}
+        sum_of_ratings = ratings.reduce(0) { |acc, rating| acc + rating }
 
         sum_of_ratings / ratings.size
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def chars_count(films, threshold)
+        films
+          .select { |film| film['rating_kinopoisk'].to_f >= threshold }
+          .map { |film| film['name'].split('').select { |char| char == 'и' }.size }
+          .reduce(0) { |sum, count_films_chars| sum + count_films_chars }
       end
     end
   end
