93ed8edec0923b391f12cccef3d8b5a54a40c628
Sergei Andreev <sergey.andreev@dualbootpartners.com>
2021-10-22 15:50:23 +0500

    Complete array tasks

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,29 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max = array[0]
+        array.each { |el| max = el > max ? el : max }
+        array.map { |el| el.positive? ? max : el }
+      end
+
+      def recursion_search(array, query, left_index, right_index)
+        return -1 if right_index < left_index
+
+        middle = (left_index + right_index) / 2
+
+        if array[middle] == query
+          middle
+        elsif array[middle] < query
+          recursion_search(array, query, middle + 1, right_index)
+        else
+          recursion_search(array, query, left_index, middle - 1)
+        end
       end
 
       def search(_array, _query)
-        0
+        left_index = 0
+        right_index = _array.length - 1
+        recursion_search(_array, _query, left_index, right_index)
       end
     end
   end
