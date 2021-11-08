c0b30ed44895238358bdf21214130749a70b0971
qa3aq <dimdac@gmail.com>
2021-11-08 19:53:37 +0500

    Added implemented methods

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,12 +4,15 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        selected_rating = array.select { |film| !film['rating_kinopoisk'].nil? && !film['country'].nil? && !film['rating_kinopoisk'].to_f.zero? }
+        rating = selected_rating.select { |film| film['country'].split(',').size >= 2 }.map { |film| film['rating_kinopoisk'].to_f }
+
+        rating.reduce(:+) / rating.size
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def chars_count(films, threshold)
+        films.select { |film| film['rating_kinopoisk'].to_f >= threshold }.map { |film| film['name'].scan('и').count }.reduce(:+)
       end
     end
   end
