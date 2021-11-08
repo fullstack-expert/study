e6faf8d952a3dc532f51922ad3ff1fdc60264067
qa3aq <dimdac@gmail.com>
2021-11-08 19:57:02 +0500

    Added implemented methods

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,26 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        array.map { |el| el > 0 ? array.max : el }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        start = 0
+        endpoint = array.size - 1
+
+        return -1 if array.empty?
+
+        while start <= endpoint
+          middle = (start + endpoint) / 2
+          return middle if array[middle] == query
+
+          if array[middle] < query
+            start = middle + 1
+          else
+            endpoint = middle - 1
+          end
+        end
+        -1
       end
     end
   end
