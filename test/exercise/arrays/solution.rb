3706a801aae9605e68009c626c27f5447874447a
Denis Andreev <denokote10@gmail.com>
2021-11-09 16:07:12 +0200

    arrays is ready

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,30 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        array.map do |y|
+          if y > 0
+             array.max
+          else
+             y
+          end
+        end
       end
 
       def search(_array, _query)
-        0
+        min = 0
+        max = _array.length - 1
+        while min <= max
+            mid = (min + max)/2
+            x = _array[mid]
+            if x == _query
+                return mid                
+            elsif _query < x
+                max = mid - 1 
+            else 
+                min = mid + 1
+            end
+        end
+        return - 1
       end
     end
   end
