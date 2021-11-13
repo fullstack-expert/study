76d577209237d8d4ccca590ad8c38f503909b831
Anastasia <anastasia.zhura@gmail.com>
2021-11-13 14:04:21 +0300

    re-write search to recursive search

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,28 +2,26 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        largest = array.inject do |memo, num|
-          memo > num ? memo : num
-        end
-
+        largest = array.inject { |memo, num| memo > num ? memo : num }
         array.map { |el| el.positive? ? largest : el }
       end
 
       def search(array, query)
-        low = 0
-        high = array.length - 1
-        while low <= high
+        recursive_search = lambda do |sorted_array, key, low, high|
           mid = (low + high) / 2
-          guess = array[mid]
-          return mid if guess == query
+          guess = sorted_array[mid]
+          return - 1 if low > high
 
-          if guess > query
-            high = mid - 1
+          return mid if guess == key
+
+          if guess > key
+            recursive_search.call(sorted_array, key, low, mid - 1)
           else
-            low = mid + 1
+            recursive_search.call(sorted_array, key, mid + 1, high)
+
           end
         end
-        -1
+        recursive_search.call(array, query, 0, array.length - 1)
       end
     end
   end
