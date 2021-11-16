5245eaa97512128823e9022eee01f8209376fbb4
Arlantir <ror@worldoflosttime.ru>
2021-11-17 00:39:54 +0300

    solving task array

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,19 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max = array.reduce { |acc, el| acc > el ? acc : el }
+
+        array.map { |el| el.positive? ? max : el }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        return -1 if array.empty?
+
+        array.each.with_index do |v, k|
+          return k.to_i if v == query
+        end
+
+        return -1
       end
     end
   end
