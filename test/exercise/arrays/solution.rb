22fb991e6b5537e684a9d01d1ec22659478206b6
Sergey Heinrich <hans.nordwand@gmail.com>
2021-11-18 12:14:43 +0300

    ver 1 (using standard functions, without recursion)

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,23 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        array.map { |i| i.positive? ? array.max : i }
       end
 
-      def search(_array, _query)
-        0
+      def search(arr, qry)
+        idx_start = 0
+        idx_end = arr.length
+        loop do
+          len = idx_end - idx_start
+          return arr[idx_start] == qry ? idx_start : -1 if len < 2
+
+          idx_cen = len / 2 + idx_start
+          if arr[idx_cen] > qry
+            idx_end = idx_cen
+          else
+            idx_start = idx_cen
+          end
+        end
       end
     end
   end
