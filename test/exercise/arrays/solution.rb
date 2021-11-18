1ee0fc08d43cff4fa28ad476a5c568119211abd8
vanushinvi <vanushinvi@gmail.com>
2021-11-18 12:42:02 +0400

    add array methods

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -6,23 +6,23 @@ module Exercise
         array.map { |element| element.positive? ? max : element }
       end
 
-      def bin_search(arr, targ, min_ind, max_ind)
-        mid_ind = (max_ind + min_ind) / 2
-        return -1 if arr.empty? || (targ > arr[max_ind]) || (targ < arr[min_ind])
+      def binary_search(array, target, min_index, max_index)
+        mid_index = (max_index + min_index) / 2
+        return -1 if array.empty? || (target > array[max_index]) || (target < array[min_index])
 
-        return mid_ind if targ == arr[mid_ind]
+        return mid_index if target == array[mid_index]
 
-        if targ > arr[mid_ind]
-          bin_search(arr, targ, mid_ind + 1, max_ind)
-        elsif targ < arr[mid_ind]
-          bin_search(arr, targ, min_ind, mid_ind - 1)
+        if target > array[mid_index]
+          binary_search(array, target, mid_index + 1, max_index)
+        elsif target < array[mid_index]
+          binary_search(array, target, min_index, mid_index - 1)
         end
       end
 
       def search(array, query)
-        min_ind = 0
-        max_ind = array.length - 1
-        bin_search(array, query, min_ind, max_ind)
+        min_index = 0
+        max_index = array.length - 1
+        binary_search(array, query, min_index, max_index)
       end
     end
   end
