f0cb8fbdb89f5d0b0a106fbb70145fbeb0597e02
vanushinvi <vanushinvi@gmail.com>
2021-11-21 18:56:40 +0400

    add mapreduce

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,12 +4,16 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(films)
+        filter_films = films.select { |film| film['country'] && film['country'].include?(',') && !film['rating_kinopoisk'].to_f.zero? }
+        raiting = filter_films.map { |film| film['rating_kinopoisk'].to_f }
+        raiting.reduce(:+) / raiting.count
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def chars_count(films, threshold)
+        list_films = films.select { |film| film['rating_kinopoisk'].to_f >= threshold }
+        array_count = list_films.map { |film| film['name'].scan('и').count }
+        array_count.sum
       end
     end
   end
